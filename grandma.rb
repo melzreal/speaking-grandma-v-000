ec8f124dea93024719c1b95fd04981a97267eff6
Melissa Real <melissa.real@hotmail.com>
2018-04-21 14:16:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   
-  if phrase!=phrase.upcase
+  if phrase == ""
+  
+  elsif 
+    phrase!=phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
     phrase = phrase.upcase
     return phrase
